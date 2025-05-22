c4c617f63d0dbc967c75065861cc1bf99423d16d
Sundari <sundarinnagraj@gmail.com>
2025-05-22 10:44:01 +0530

    first commit

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import{View}from 'react-native';
+import  Welcome from './src/welcome';
+const app=()=>{
+  return(
+    <View>
+     <Welcome/>
+     <Welcome/>
+    </View>
+  );
+};
+export default app;
